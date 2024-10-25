2fc86da49647df58d4ca6f57b5683e286435247d
Benjamin TURPIN <benjamin.turpin@etu.umontpellier.fr>
2024-10-25 19:32:10 +0200

    feat: ajout des bannières

--- src/Modele/DataObject/Coach.php
+++ src/Modele/DataObject/Coach.php
@@ -2,25 +2,34 @@
 
 namespace App\PlayToWin\Modele\DataObject;
 
+use App\PlayToWin\Lib\MessageFlash;
 use DateTime;
 
 class Coach extends Utilisateur {
     private string $biographie;
+    private static string $pathBannieres = "ressources/img/uploads/coach/bannieres/";
 
     public function __construct(Utilisateur $utilisateur, string $biographie) {
         parent::__construct($utilisateur->getId(),$utilisateur->getNom(),$utilisateur->getPrenom(),$utilisateur->getPseudo(),$utilisateur->getEmail(),$utilisateur->getEmailAValider(),$utilisateur->getNonce(),$utilisateur->getDateNaissance(),$utilisateur->getMdpHache(),$utilisateur->isAdmin());
         $this->biographie = $biographie;
     }
-
-    public function getBiographie(): string {
+    public function getBiographie(): string
+    {
         return $this->biographie;
     }
-
-    public function setBiographie(string $biographie): void {
+    public function setBiographie(string $biographie): void
+    {
         $this->biographie = $biographie;
     }
-
-    public function getBanniere(): string {
-        return "img/uploads/coach/banniere/".$this->getId().".png";
+    public function getBannierePath(): string
+    {
+        $ext = "";
+        if(file_exists(__DIR__ ."/../../../".$this::$pathBannieres.$this->getId().".png")){
+            $ext = ".png";
+        } else if (file_exists(__DIR__ ."/../../../".$this::$pathBannieres.$this->getId().".jpg")){
+            $ext = ".jpg";
+        } else{
+        }
+        return $this::$pathBannieres.$this->getId().$ext;
     }
 }
