99008a276db126ba71b0dfcf507461a2c49848c2
michauxa <alexis.michaux-kinet@etu.umontpellier.fr>
2024-10-27 10:08:42 +0100

    fix: creation exemplaire / commande

--- BaseDeDonnees/Scripts/Alter.sql
+++ BaseDeDonnees/Scripts/Alter.sql
@@ -6,7 +6,7 @@ ADD CONSTRAINT fk_Coachs_Utilisateurs FOREIGN KEY (idCoach) REFERENCES p_Utilisa
 
 -- Table p_Commandes
 ALTER TABLE p_Commandes
-ADD CONSTRAINT fk_Commande_Utilisateurs FOREIGN KEY (idUtilisateur) REFERENCES p_Utilisateurs(idUtilisateur) ON DELETE CASCADE;
+ADD CONSTRAINT fk_Commandes_Utilisateurs FOREIGN KEY (idUtilisateur) REFERENCES p_Utilisateurs(idUtilisateur) ON DELETE CASCADE;
 
 -- Table p_Services
 ALTER TABLE p_Services
