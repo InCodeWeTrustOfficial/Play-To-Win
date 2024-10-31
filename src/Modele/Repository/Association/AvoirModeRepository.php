4016de05da3436c6078e9cdb149749f5ab6b12ef
Benjamin TURPIN <benjamin.turpin@etu.umontpellier.fr>
2024-10-31 20:03:41 +0100

    feat: nouveaux repo des inclusions jouer

---
+++ src/Modele/Repository/Association/AvoirModeRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\PlayToWin\Modele\Repository\Association;
+
+
+use App\PlayToWin\Modele\Repository\Single\JeuRepository;
+use App\PlayToWin\Modele\Repository\Single\ModeDeJeuRepository;
+
+class AvoirModeRepository extends AbstractAssociationRepository {
+
+    protected function getNomsClePrimaire(): array
+    {
+        return array((new ModeDeJeuRepository())->getNomClePrimaire(),(new JeuRepository())->getNomClePrimaire());
+    }
+
+    protected function formatTableauSQL(array $objet): array
+    {
+        return array(
+          ":cle1Tag" => $objet[0],
+          ":cle2Tag" => $objet[1]
+        );
+    }
+
+    protected function getNomTable(): string
+    {
+        return "p_avoirMode";
+    }
+
+    protected function getNomsColonnes(): array
+    {
+        return $this->getNomsClePrimaire();
+    }
+}
---
+++ src/Modele/Repository/Association/SeClasserRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\PlayToWin\Modele\Repository\Association;
+
+class SeClasserRepository extends AbstractAssociationRepository {
+
+    protected function getNomsClePrimaire(): array
+    {
+        // TODO: Implement getNomsClePrimaire() method.
+    }
+
+    protected function formatTableauSQL(array $objet): array
+    {
+        // TODO: Implement formatTableauSQL() method.
+    }
+
+    protected function getNomTable(): string
+    {
+        return "p_seClasser";
+    }
+
+    protected function getNomsColonnes(): array
+    {
+        // TODO: Implement getNomsColonnes() method.
+    }
+}
