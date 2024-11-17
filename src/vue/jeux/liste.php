c544aeeb7c0143e3abdf1c4b04bf8b46f2b0a85a
Benjamin TURPIN <benjamin.turpin@etu.umontpellier.fr>
2024-11-17 08:28:45 +0100

    feat: affichage liste jeux

---
+++ src/vue/jeux/liste.php
@@ -0,0 +1,25 @@
+<div class="contenu">
+    <div class="texte">
+        <h2>Besoin d'un <span id="coaching">coaching</span> ?</h2>
+        <p>Découvre la sélection de jeux que nous te proposons afin de te rendre davantage compétitif dans les modes
+            classés !</p>
+    </div>
+    <div class="separator"></div>
+
+    <?php
+
+    use App\PlayToWin\Modele\DataObject\Jeu;
+
+    /** @var Jeu[] $jeux */
+    ?>
+    <div class="conteneur-jeux">
+        <?php
+        foreach ($jeux as $jeu) {
+            echo '<div class="jeu-carte">';
+            echo '<a href="../web/controleurFrontal.php?controleur=coach&action=afficherListe&jeu='.rawurlencode($jeu->getCodeJeu()).'"><img class="jeux" src="../' . $jeu->getPathLogo() . '" alt="icon">';
+            echo '<p>' . $jeu->getNomJeu() . '</p></a>';
+            echo '</div>';
+        }
+        ?>
+    </div>
+</div>
