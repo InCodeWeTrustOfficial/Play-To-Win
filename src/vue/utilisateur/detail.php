fe4f868dddd8ee67d5d3f518817b18ac7e35f7ad
Benjamin TURPIN <benjamin.turpin@etu.umontpellier.fr>
2024-11-17 22:23:19 +0100

    feat: page profil

--- src/vue/utilisateur/detail.php
+++ src/vue/utilisateur/detail.php
@@ -20,6 +20,9 @@ $dateNaissanceHTML = htmlspecialchars($utilisateur->getDateNaissance()->format("
 $emailHTML = htmlspecialchars($utilisateur->getEmail());
 $avatarHTML = htmlspecialchars($utilisateur->getAvatarPath());
 
+$langues = (new ParlerRepository())->recupererLangues($utilisateur->getId());
+$jouer = (new JouerRepository())->recupererModeJeuClassement($utilisateur->getId());
+
 $aValideEmail = $utilisateur->getEmail() !== "";
 $estAdmin = ConnexionUtilisateur::estAdministrateur();
 $estBonUtilisateur = $estAdmin || (ConnexionUtilisateur::estConnecte() && ConnexionUtilisateur::estUtilisateur($utilisateur->getId()));
@@ -42,78 +45,67 @@ $estCoach = (new CoachRepository())->estCoach($utilisateur->getId());
         </div>
 
         <div class="infos-generales">
-            <p>id : <?=$idHTML?></p>
-            <p>Nom : <?=$nomHTML?></p>
-            <p>Prenom : <?=$prenomHTML?></p>
-            <p>Pseudo : <?=$pseudoHTML?></p>
-            <p>Email : <?=$emailHTML?></p>
-            <p>Date de naissance : <?=$dateNaissanceHTML?></p>
+            <div class="basiques">
+                <p>id : <?= $idHTML ?></p>
+                <p>Nom : <?= $nomHTML ?></p>
+                <p>Prenom : <?= $prenomHTML ?></p>
+                <p>Pseudo : <?= $pseudoHTML ?></p>
+                <p>Email : <?= $emailHTML ?></p>
+                <p>Date de naissance : <?= $dateNaissanceHTML ?></p>
+            </div>
+            <div class="specifiques">
+                <div class="langues-utilisateur">
+                    <?php
+                    if ($langues === null) {
+                        echo '<p>Aucune langue enregistrée.</p>';
+                    } else{
+                        /** @var Langue $l */
+                        foreach ($langues as $l){
+                            echo '<p class="langu"><img src="../'.$l->getDrapeauPath().'" alt="Drapeau">';
+                            if($estBonUtilisateur){
+                                echo '<a href="../web/controleurFrontal.php?controleur=langue&action=supprimerLangue&id=' . $idURL . '&lang='.$l->getCodeAlpha().'">X</a>';
+                            }
+                            echo '</p>';
+                        }
+                    }
+                    ?>
+                    <a <?php if (!$estBonUtilisateur) echo 'class="cache"'?> href="../web/controleurFrontal.php?controleur=langue&action=afficherFormulaireAjout&id=<?=$idURL?>">Nouvelle langue ?</a>
+                </div>
+            </div>
         </div>
+        <div class="jeux-utilisateur">
+            <?php
+            if($jouer === null){
+                echo '<p>Aucun jeu</p>';
+            } else{
+                foreach ($jouer as $ligne){
+                    /** @var ClassementJeu $classJeu */
+                    $classJeu = (new SeClasserRepository())->recupererDepuisJouer($ligne);
+                    echo '<p class="ligne-jeu">
+                                    <small>'.$ligne[1]->getNomMode().'</small>
+                                    <img src="../'.$ligne[0]->getPathLogo().'" alt="'.$ligne[0]->getNomJeu().'">
+                                    <img class="classement" src="../'.$classJeu->getClassPath().'" alt="Classement">';
+                    if($estBonUtilisateur){
+                        echo '<a href="../web/controleurFrontal.php?controleur=jouer&action=afficherModifJouer&id=' . $idURL . '&jeu='.$ligne[0]->getCodeJeu().'&mode='.$ligne[1]->getNomMode().'"> (Modif) </a>
+                                <a href="../web/controleurFrontal.php?controleur=jouer&action=supprimerJouer&id=' . $idURL . '&jeu='.$ligne[0]->getCodeJeu().'&mode='.$ligne[1]->getNomMode().'">X</a>';
+                    }
+                    echo "</p>";
+                }
+            }
+            ?>
+            <a <?php if (!$estBonUtilisateur) echo 'class="cache"'?> href="../web/controleurFrontal.php?controleur=jouer&action=afficherFormulaireJouer&id=<?=$idURL?>">Nouveau jeu?</a>
+        </div>
+        <?php
+        if($estBonUtilisateur){
+            echo '<div class="lienMAJ">';
+            echo '    <a href = "../web/controleurFrontal.php?controleur=utilisateur&action=afficherFormulaireMiseAJour&id='.$idURL.'">Modifier des informations ?</a>';
+            echo '    <a href = "../web/controleurFrontal.php?controleur=utilisateur&action=supprimer&id=' . $idURL . '">Supprimer le compte</a>';
+            echo '</div>';
+        }
+        ?>
     </div>
-
     <div class="devCoach">
         <a <?php if (!$estCoach) echo 'class="cache"'?> href = "../web/controleurFrontal.php?controleur=coach&action=afficherDetail&id=<?=$idURL?>">Voir la page coach</a>
-        <a <?php if (!(!$estCoach || !$estBonUtilisateur)) echo 'class="cache";'?> href = "../web/controleurFrontal.php?controleur=coach&action=afficherFormulaireCreation&id=' . $idURL . '"> je souhaite devenir coach...</a>
+        <a <?php if ($estCoach || !$estBonUtilisateur) echo 'class="cache"'?> href = "../web/controleurFrontal.php?controleur=coach&action=afficherFormulaireCreation&id=<?=$idURL?>"> je souhaite devenir coach...</a>
     </div>
-
 </div>
-
-<?php
-echo '<p>Langues parlées: ';
-$langues = (new ParlerRepository())->recupererLangues($utilisateur->getId());
-if($langues == null){
-    echo 'Aucune :(';
-} else{
-    /** @var Langue $l */
-    foreach ($langues as $l){
-        echo '<p> <img src="../'.$l->getDrapeauPath().'" alt="Drapeau" style="width: 40px; height: 26px; object-fit: cover;">';
-        if(ConnexionUtilisateur::estUtilisateur($utilisateur->getId()) || ConnexionUtilisateur::estAdministrateur()){
-            echo '<a href="../web/controleurFrontal.php?controleur=langue&action=supprimerLangue&id=' . $idURL . '&lang='.$l->getCodeAlpha().'">(-)</a></p>';
-        }
-  echo '</p>';
-    }
-}
-if(ConnexionUtilisateur::estUtilisateur($utilisateur->getId()) || ConnexionUtilisateur::estAdministrateur()){
-    echo '<p> Ajouter une nouvelle langue ?';
-    echo '<a href="../web/controleurFrontal.php?controleur=langue&action=afficherFormulaireAjout&id=' . $idURL . '">Cliquez ici</a></p>';
-}
-echo '</p>';
-
-echo '<p> Jeux joués avec leur mode:';
-$jouer = (new JouerRepository())->recupererModeJeuClassement($utilisateur->getId());
-if($jouer == null){
-    echo 'Aucun jeu :(';
-} else{
-    foreach ($jouer as $ligne){
-        /** @var ClassementJeu $classJeu */
-        $classJeu = (new SeClasserRepository())->recupererDepuisJouer($ligne);
-        echo "<p>";
-        echo $ligne[0]->getNomJeu();
-        echo $ligne[1]->getNomMode();
-        echo $classJeu->getClassement()->getNomClassement();
-        echo '<img src="../'.$classJeu->getClassPath().'" alt="Classement" style="width: 30px; height: 30px; object-fit: cover;">';
-        if(ConnexionUtilisateur::estUtilisateur($utilisateur->getId()) || ConnexionUtilisateur::estAdministrateur()){
-            echo ' <a href="../web/controleurFrontal.php?controleur=jouer&action=afficherModifJouer&id=' . $idURL . '&jeu='.$ligne[0]->getCodeJeu().'&mode='.$ligne[1]->getNomMode().'"> (Modif) </a>
-            <a href="../web/controleurFrontal.php?controleur=jouer&action=supprimerJouer&id=' . $idURL . '&jeu='.$ligne[0]->getCodeJeu().'&mode='.$ligne[1]->getNomMode().'">(-)</a></p>';
-        }
-        echo "</p>";
-    }
-}
-if(ConnexionUtilisateur::estUtilisateur($utilisateur->getId()) || ConnexionUtilisateur::estAdministrateur()){
-    echo '<p> Ajouter un nouveau jeu ?';
-    echo '<a href="../web/controleurFrontal.php?controleur=jouer&action=afficherFormulaireJouer&id=' . $idURL . '">Cliquez ici</a></p>';
-}
-echo '<p>
-<img src="../'.$utilisateur->getAvatarPath().'" alt="Photo de profil" style="width: 70px; height: 70px; object-fit: cover;" 
-     onerror="this.onerror=null; this.src=\'../ressources/img/defaut_pp.png\';">
-</p>';
-
-
-if (ConnexionUtilisateur::estUtilisateur($utilisateur->getId()) || ConnexionUtilisateur::estAdministrateur()) {
-    echo '<p>
-    <a href = "../web/controleurFrontal.php?controleur=utilisateur&action=afficherFormulaireMiseAJour&id=' . $idURL . '"> (ModifICI) </a>';
-    echo '<a href = "../web/controleurFrontal.php?controleur=utilisateur&action=supprimer&id=' . $idURL . '"> (-)</a>
-    </p>';
-
-}
-
