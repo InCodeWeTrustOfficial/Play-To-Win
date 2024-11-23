bd4f706e5dba967b2402723511f807acc3248048
michauxa <alexis.michaux-kinet@etu.umontpellier.fr>
2024-11-23 19:24:45 +0100

    feature: logo dans nav bar et logo dans onglet

--- src/vue/vueGenerale.php
+++ src/vue/vueGenerale.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <link rel="icon" href="../ressources/img/icone/play-to-win-logo.png" type="image/png">
     <link rel="stylesheet" href="../ressources/css/nav_bar.css">
     <link rel="stylesheet" href="../ressources/css/background.css">
     <link rel="stylesheet" href="../ressources/css/footer.css">
@@ -28,9 +29,9 @@
 <body>
 <header>
     <nav class="navbar">
-        <a href="controleurFrontal.php" class="nav-link">
+        <a href="controleurFrontal.php" class="logo-link">
             <img src="../ressources/img/icone/play-to-win-logo.png" alt="Logo">
-            Home
+            Play To Win
         </a>
 
         <div class="nav-center">
